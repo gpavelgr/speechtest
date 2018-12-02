023bdc210e50b7eb801c59fed5a465ed2a16f58c
paul <paul.okopny@gmail.com>
2018-12-02 06:17:31 +0300

    initial commi

--- src/prototype.py
+++ src/prototype.py
@@ -1,2 +1,15 @@
-# Prototype using behave
-#
+list_of_tasks = []
+
+
+def recognize_commmand(command):
+    pass
+
+
+def put_task_in_list(task):
+    list_of_tasks.append(task)
+
+
+def execute_task(task):
+    eval(task)
+
+
